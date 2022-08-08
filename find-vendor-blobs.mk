c615cad7ad23480e5cbb632ed03e320e327553bd
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-08-08 10:44:19 +0900

    universal7885: the initial of supports treble device.
    Change-Id: If3836237134707ebce0a97a6931aa0c25e4c626e

--- find-vendor-blobs.mk
+++ find-vendor-blobs.mk
@@ -14,9 +14,14 @@
 # limitations under the License.
 #
 
+# get treble info from BoardConfig.mk
+include $(DEVICE_PATH)/BoardConfig.mk
+
 define find-vendor-blobs
     $(foreach vendor_file, $(shell find $(1) -type f | sed -n 's|^$(1)/||p'), \
     	$(if $(strip $(findstring $(vendor_file):$(vendor_file),$(PRODUCT_COPY_FILES))), \
     		$(NOOP), \
-    		$(1)/$(vendor_file):$(vendor_file)))
+                $(if $(filter system/vendor, $(TARGET_COPY_OUT_VENDOR)), \
+                $(1)/$(vendor_file):$(vendor_file), \
+                $(1)/$(vendor_file):$(strip $(subst system/, vendor/, $(subst system/vendor, vendor, $(vendor_file)))))))
 endef
